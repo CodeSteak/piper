c8e3ea82452720d11ada6c729607f47f75b01157
CodeSteak <codesteak@shellf.art>
2022-10-07 22:13:24 +0200

    impl pipe

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -3,9 +3,11 @@ extern crate test;
 
 mod bip39;
 mod crypto;
+mod pipe;
 mod tar_hash;
 mod tar_password;
 
 pub use crypto::*;
+pub use pipe::*;
 pub use tar_hash::*;
 pub use tar_password::*;
---
+++ common/src/pipe.rs
@@ -0,0 +1,54 @@
+use std::io::Read;
+
+// TODO: Optimize this
+pub fn create_pipe() -> (PipeReader, PipeWriter) {
+    let (sender, receiver) = std::sync::mpsc::sync_channel(64);
+    (
+        PipeReader {
+            buffer: vec![],
+            receiver,
+        },
+        PipeWriter { written: 0, sender },
+    )
+}
+
+pub struct PipeReader {
+    buffer: Vec<u8>,
+    receiver: std::sync::mpsc::Receiver<Vec<u8>>,
+}
+
+pub struct PipeWriter {
+    written: u64,
+    sender: std::sync::mpsc::SyncSender<Vec<u8>>,
+}
+
+impl Read for PipeReader {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        if self.buffer.is_empty() {
+            match self.receiver.recv() {
+                Ok(v) => {
+                    self.buffer = v;
+                }
+                Err(_) => return Ok(0),
+            };
+        }
+        let n = std::cmp::min(buf.len(), self.buffer.len());
+        buf[..n].copy_from_slice(&self.buffer[..n]);
+        self.buffer.drain(..n);
+        Ok(n)
+    }
+}
+
+impl std::io::Write for PipeWriter {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.sender
+            .send(buf.to_vec())
+            .map_err(|_| std::io::Error::new(std::io::ErrorKind::Other, "send error"))?;
+        self.written += buf.len() as u64;
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
